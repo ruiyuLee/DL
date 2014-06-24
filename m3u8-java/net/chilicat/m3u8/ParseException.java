37b6e88551115c443d0771fa950bdabd5535e289
allnet@live.cn <allnet@live.cn>
2014-06-24 20:04:31 +0800

    添加m3u8解析代码。

---
+++ m3u8-java/net/chilicat/m3u8/ParseException.java
@@ -0,0 +1,37 @@
+package net.chilicat.m3u8;
+
+/**
+ * @author dkuffner
+ */
+public class ParseException extends Exception {
+	private static final long serialVersionUID = 3563213770414066457L;
+	private final String line;
+	private final int lineNumber;
+
+	public ParseException(String line, int lineNumber, Throwable cause) {
+		super(cause);
+		this.line = line;
+		this.lineNumber = lineNumber;
+	}
+
+	public ParseException(String line, int lineNumber, String message) {
+		super(message);
+		this.line = line;
+		this.lineNumber = lineNumber;
+	}
+
+	public String getLine() {
+		return line;
+	}
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	@Override
+	public String getMessage() {
+		return "Error at line " + getLineNumber() + ": " + getLine() + "\n"
+				+ super.getMessage(); // To change body of overridden methods
+										// use File | Settings | File Templates.
+	}
+}
